b2dd1f18f55969fcf12c3f1ce61ea128f83ea017
AshenLad <t_welad@163.com>
2024-07-12 20:27:35 +0800

    model.py添加注释

--- pytorch_classification/train_multi_GPU/model.py
+++ pytorch_classification/train_multi_GPU/model.py
@@ -3,6 +3,11 @@ import torch
 
 
 class BasicBlock(nn.Module):
+    '''
+    18层与34层resNet用到的基础模块，每个模块只有两层卷积
+    downsample是模块间的skip connection间的连接方式，stride=1时模块的input与output维度一致可以直接相加
+    stride=2时模块的input与output维度不一致，需要把input用1*1的filter修改维度后与output相加
+    '''
     expansion = 1
 
     def __init__(self, in_channel, out_channel, stride=1, downsample=None):
@@ -35,6 +40,12 @@ class BasicBlock(nn.Module):
 
 
 class Bottleneck(nn.Module):
+    '''
+    50层、101层与34层resNet用到的基础模块，每个模块有三层卷积
+    其中每个模块前两层卷积filter的个数一致，第三层卷积filter的个数是前两层的4倍，故这里expansion=4
+    与BasicBlock一致，Bottleneck同样有两种skip connection连接方式
+    stride=1时的直接连接，stride=2（这里的stride=2是指第二层卷积，第一层与第三层的stride=1）时的转换维度后连接
+    '''
     expansion = 4
 
     def __init__(self, in_channel, out_channel, stride=1, downsample=None):
@@ -82,15 +93,20 @@ class ResNet(nn.Module):
         self.include_top = include_top
         self.in_channel = 64
 
+        #残差模块前的处理
         self.conv1 = nn.Conv2d(3, self.in_channel, kernel_size=7, stride=2,
                                padding=3, bias=False)
         self.bn1 = nn.BatchNorm2d(self.in_channel)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        #不管哪种残差网络都有4层（每层里包含多个残差模块）
         self.layer1 = self._make_layer(block, 64, blocks_num[0])
-        self.layer2 = self._make_layer(block, 128, blocks_num[1], stride=2)
+        self.layer2 = self._make_layer(block, 128, blocks_num[1], stride=2)#第二层开始每一层的第一个模块都是转换维度后连接
         self.layer3 = self._make_layer(block, 256, blocks_num[2], stride=2)
         self.layer4 = self._make_layer(block, 512, blocks_num[3], stride=2)
+
+        #include_top表示进一步处理
         if self.include_top:
             self.avgpool = nn.AdaptiveAvgPool2d((1, 1))  # output size = (1, 1)
             self.fc = nn.Linear(512 * block.expansion, num_classes)
@@ -101,6 +117,7 @@ class ResNet(nn.Module):
 
     def _make_layer(self, block, channel, block_num, stride=1):
         downsample = None
+        # 第二层开始每一层的第一个模块都是转换维度后连接，也就是2～4层的第一个模块一定是stride=2，如果是Bottleneck模块的话一定是self.in_channel != channel * block.expansion
         if stride != 1 or self.in_channel != channel * block.expansion:
             downsample = nn.Sequential(
                 nn.Conv2d(self.in_channel, channel * block.expansion, kernel_size=1, stride=stride, bias=False),
@@ -111,7 +128,7 @@ class ResNet(nn.Module):
         self.in_channel = channel * block.expansion
 
         for _ in range(1, block_num):
-            layers.append(block(self.in_channel, channel))
+            layers.append(block(self.in_channel, channel))#除了第一个模块外，其余的模块stride默认是1
 
         return nn.Sequential(*layers)
 
@@ -140,3 +157,10 @@ def resnet34(num_classes=1000, include_top=True):
 
 def resnet101(num_classes=1000, include_top=True):
     return ResNet(Bottleneck, [3, 4, 23, 3], num_classes=num_classes, include_top=include_top)
+
+
+if __name__ == "__main__":
+    x = torch.randn(1, 3, 224, 224)
+    model = resnet34()
+    output = model(x)
+    print(output.shape)
