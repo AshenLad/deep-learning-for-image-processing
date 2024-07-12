f6292f0c409fbd00e8038bfa9cb038caf8d20e84
AshenLad <t_welad@163.com>
2024-07-12 20:37:22 +0800

    删除了requirement.txt

--- pytorch_classification/train_multi_GPU/model_rewrite.py
+++
@@ -1,7 +0,0 @@
-import torch
-import torch.nn as nn
-
-class BasicBlock(nn.Module):
-    expansion = 1
-    def __init__(self, in_channel, out_channel, stride=1, downsample=None):
-        super(BasicBlock, self).__init__()
